1b60798cdb998f48eaebbf68afab325cddebf96c
e-victoria <evictoria@protonmail.com>
2020-04-22 12:35:19 +0200

    Update coins amount, add getCoinsAmount function

--- public/js/VendingMachine.ts
+++ public/js/VendingMachine.ts
@@ -1,11 +1,22 @@
 export default class VendingMachine {
+    private coinsAmount: number;
+
+    constructor() {
+        this.coinsAmount = 0;
+    }
+
     insertCoin(coin: number): boolean {
         let answer: boolean = false;
         
         if (coin === 0.1 || coin === 0.05 || coin === 0.25) {
+            this.coinsAmount += coin;
             answer = true;
         }
 
         return answer;
+    }
+
+    getCoinsAmount(): number {
+        return this.coinsAmount;
     }
 }
