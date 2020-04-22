24ec8e5060f9bfa86faa8d096ba8eecdbf0b7b6f
e-victoria <evictoria@protonmail.com>
2020-04-22 11:59:28 +0200

    Fix tests

--- spec/tests/VendingMachineSpec.js
+++ spec/tests/VendingMachineSpec.js
@@ -23,21 +23,21 @@ describe("VendingMachine", function () {
 
     it("should accept a nickel", function () {
         const actual = vendingMachine.insertCoin(0.05);
-        const expected = false;
+        const expected = true;
 
         expect(actual).toEqual(expected);
     });
 
     it("should accept a dime", function () {
         const actual = vendingMachine.insertCoin(0.1);
-        const expected = false;
+        const expected = true;
 
         expect(actual).toEqual(expected);
     });
 
     it("should accept a quarter", function () {
         const actual = vendingMachine.insertCoin(0.25);
-        const expected = false;
+        const expected = true;
 
         expect(actual).toEqual(expected);
     });
