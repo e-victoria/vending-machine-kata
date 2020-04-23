e4e30b9ea7d28320c7e1dcc8402f3ccb2f35c3f7
e-victoria <evictoria@protonmail.com>
2020-04-23 15:59:35 +0200

    Test giving change back

--- spec/tests/VendingMachineSpec.js
+++ spec/tests/VendingMachineSpec.js
@@ -83,4 +83,15 @@ describe("VendingMachine", function () {
         expect(actual).toEqual(expected);
     });
 
+    it("return give change back", () => {
+        vendingMachine.setCoinsAmount(1);
+        for (let i = 0; i < 3; i++) {
+            vendingMachine.insertCoin(0.25);
+        }
+        const actual = vendingMachine.giveMoneyBack();
+        const expected = 0.25;
+
+        expect(actual).toEqual(expected);
+    });
+
 })
